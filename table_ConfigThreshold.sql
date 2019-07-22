b5e38b885124dcefea28813c4b567e01959cdb52
selectify <ynsuyanik@gmail.com>
2019-07-22 09:24:59 +0300

    Update table_ConfigThreshold.sql

--- table_ConfigThreshold.sql
+++ table_ConfigThreshold.sql
@@ -32,3 +32,18 @@ SET ANSI_PADDING OFF
 GO
 
 
+
+DECLARE @customername varchar(100)=''
+DECLARE @profilename varchar(100)=''
+SET IDENTITY_INSERT ConfigThreshold ON
+insert into ConfigThreshold (ID,customer_name,alert_group,is_active,[description],[To],CC,BCC,profilename,[value],last_check_date,last_mail_send)
+VALUES
+(1,@customername,'Job Failed',1,'Hata alan job.','dba@silikonakademi.com','','',@profilename,0,getdate()-1,getdate()-1),
+(2,@customername,'Disk Size',1,'0-100 arasi deger alan free space disk orani.','dba@silikonakademi.com','','',@profilename,20,getdate()-1,getdate()-1),
+(3,@customername,'CPU',1,'0-100 arasi deger alan cpu.','dba@silikonakademi.com','','',@profilename,80,getdate()-1,getdate()-1),
+(4,@customername,'is_percent_growth',1,'bit deger alan is_percent_growth=0','dba@silikonakademi.com','','',@profilename,0,getdate()-1,getdate()-1),
+(5,@customername,'TempDB Size MB',1,'KB deger alan tempdb size.','dba@silikonakademi.com','','',@profilename,350000,getdate()-1,getdate()-1),
+(6,@customername,'AlwaysOn Latency',1,'Dakika deger alan AlwaysOn Latency.','dba@silikonakademi.com','','',@profilename,30,getdate()-1,getdate()-1),
+(7,@customername,'Weekly Report',1,'Haftalik performans stats ve wait statistics raporlari.','dba@silikonakademi.com','','',@profilename,0,getdate()-1,getdate()-1),
+(8,@customername,'Log File Size',1,'Log File büyüklügü Data File''in %75 veya üzeri ise.','dba@silikonakademi.com','','',@profilename,75,getdate()-1,getdate()-1)
+SET IDENTITY_INSERT ConfigThreshold OFF
